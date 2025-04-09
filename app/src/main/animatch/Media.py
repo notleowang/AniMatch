cdcf4401be55afa228e2a4579a9178b7a11129e8
notleowang <leowangmessages@gmail.com>
2025-04-08 18:41:09 -0700

    Refactored

--- app/src/main/animatch/Media.py
+++ app/src/main/animatch/Media.py
@@ -10,8 +10,9 @@ class Media:
         # self.anime: Anime = None
         # self.manga: Manga = None
 
-        self.title_english: str = None
-        self.title_romaji: str = None
+        self.title: dict = None
+        self.title_english: str = ""
+        self.title_romaji: str = ""
         self.genres: list[str] = None
         self.tags: list[str] = None
         self.description: str = None
@@ -20,19 +21,22 @@ class Media:
 
         # ======= CONSTRUCT FIELDS =======
         self.id = api_media.get('id') # Should be non-null based off GraphQL Schema
-        if api_media.get('title', None):
-            self.title_english = api_media.get('title').get('english', "")
-            self.title_romaji = api_media.get('title').get('romaji', "")
+        self.title = api_media.get('title', None)
+        if self.title:
+            self.title_english = self.title.get('english', "")
+            self.title_romaji = self.title.get('romaji', "")
         self.genres = api_media.get('genres', [])
-        self.tags = self._construct_tags(api_media.get('tags'))
+        self.tags = self._construct_tags(api_media.get('tags', []))
         self.description = api_media.get('description', "")
 
-    def _construct_tags(api_media_tags):
-        if not api_media_tags:
+    def _construct_tags(self, api_media_tags_list):
+        if not api_media_tags_list:
             return []
 
         result = []
-        for tag_name in api_media_tags:
-            result.append(tag_name)
+        for tag_dict in api_media_tags_list:
+            tag_name = tag_dict.get('name')
+            if tag_name:
+                result.append(tag_name)
 
         return result
